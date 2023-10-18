10ecafe2f1a85eff5a1edd538ce6847f5d0ba38e
woadey <ssmits@asu.edu>
2023-10-18 17:42:23 -0400

    Working

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 #### Install Packages
 install () {
-    if [[ $(dpkg -s $1 &> /dev/null) -eq 0 ]]
+    if dpkg -l | grep -q "^ii\s*$1 "
     then
         echo "[-] '$1' is already installed! Skipping..."
     else
@@ -24,12 +24,48 @@ prompt () {
     fi
 }
 
+#### Install GitHub Submodules
 echo "==================== Installing submodules ==================="
 cd $SCRIPT_DIR
 git submodule update --init --recursive
 git config --global user.email ssmits@asu.edu
 git config --global user.name woadey
 cd - &>/dev/null
+
+
+
+#### Copy dotfiles
+echo -e "\n========================== dotfiles =========================="
+prompt "upload dotfiles"
+
+if [[ $prompt_result -eq 1 ]]
+then
+    echo "[*] Symlinking dotfiles to '$(eval echo ~$USER)'"
+    FILES_TO_SYMLINK=$(find $SCRIPT_DIR/dotfiles -maxdepth 1 -name ".*" -not -name .git)
+    OLD_VIM_DIR=$HOME/.vim
+
+    # not symlink file && is directory file
+    if [[ ! -L $OLD_VIM_DIR ]] && [[ -d $OLD_VIM_DIR ]]
+    then
+        echo "[!] Removing old '.vim' directory..."
+        sudo rm -rf $HOME/.vim
+    fi
+
+    # overwrite symlinks
+    for file in $FILES_TO_SYMLINK; do
+        if [[ $(readlink -f $HOME/$(basename $file)) == $file ]]
+        then
+            echo -e "\t[!] '$HOME/$(basename $file)' already properly linked. Skipping..."
+        else
+            echo -e "\t[+] '$HOME/$(basename $file)' -> '$file'"
+            ln -s -f $file $HOME/$(basename $file)
+        fi  
+    done
+else
+    echo "[-] Skipping..."
+fi
+
+
 
 #### setup
 echo -e "\n========================= init setup ========================="
@@ -47,6 +83,8 @@ then
 else
     echo "[-] Skipping..."
 fi
+
+
 
 #### zsh / powerlevel 10k
 echo -e "\n==================== zsh / powerlevel 10k ===================="
@@ -109,93 +147,6 @@ else
 fi
 
 
-#### Gnome Customizations
-echo -e "\n==================== Gnome Customizations ===================="
-prompt "install Gnome Customizations"
-
-if [[ $prompt_result -eq 1 ]]
-then
-    install "gnome-tweaks"
-    install "papirus-icon-theme"
-    install "grub-customizer"
-    install "flameshot"
-    install "gnome-shell-extensions"
-    $SCRIPT_DIR/gogh/themes/vs-code-dark-plus.sh 
-
-    # Check if repo already hasn't added
-    if [[ $(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep papirus/papirus &> /dev/null) -eq 1 ]]
-    then
-        echo "[*] Adding repo 'ppa:papirus/papirus'"
-        sudo add-apt-repository ppa:papirus/papirus
-    fi
-
-    if [[ -d "/usr/share/themes/Orchis" ]] # lazy check
-    then 
-        echo "[-] 'Orchis-theme' is already installed! Skipping..."
-    else
-        echo "[*] Installing 'Orchis-theme'"
-        sudo $SCRIPT_DIR/Orchis-theme/install.sh -d /usr/share/themes --tweaks compact &> /dev/null
-    fi
-
-    DEST_DIR="/usr/share/icons/"
-    if [[ -d $DEST_DIR/Vimix-cursors ]] # lazy check
-    then
-        echo "[-] 'Vimix-cursors' is already installed! Skipping..."
-    else
-        echo "[*] Installing 'Vimix-cursors'"
-        sudo cp -r $SCRIPT_DIR/Vimix-cursors/dist $DEST_DIR/Vimix-cursors
-        sudo cp -r $SCRIPT_DIR/Vimix-cursors/dist-white $DEST_DIR/Vimix-white-cursors
-    fi
-
-    if [[ -d "/usr/share/grub/themes/tela" ]] # lazy check
-    then
-        echo "[-] 'grub2-themes' is already installed! Skipping..."
-    else
-        echo "[*] Installing 'grub2-themes'"
-        sudo $SCRIPT_DIR/grub2-themes/install.sh -s 1080p -t tela &> /dev/null
-    fi
-    
- 
-    # TODO add support for:
-        # https://extensions.gnome.org/extension/1503/tray-icons/
-        # https://extensions.gnome.org/extension/921/multi-monitors-add-on/
-        # https://extensions.gnome.org/extension/307/dash-to-dock/
-        # https://extensions.gnome.org/extension/906/sound-output-device-chooser/
-else
-    echo "[-] Skipping..."
-fi
-
-
-#### Copy dotfiles
-echo -e "\n========================== dotfiles =========================="
-prompt "upload dotfiles"
-
-if [[ $prompt_result -eq 1 ]]
-then
-    echo "[*] Symlinking dotfiles to '$(eval echo ~$USER)'"
-    FILES_TO_SYMLINK=$(find $SCRIPT_DIR/dotfiles -maxdepth 1 -name ".*" -not -name .git)
-    OLD_VIM_DIR=$HOME/.vim
-
-    # not symlink file && is directory file
-    if [[ ! -L $OLD_VIM_DIR ]] && [[ -d $OLD_VIM_DIR ]]
-    then
-        echo "[!] Removing old '.vim' directory..."
-        sudo rm -rf $HOME/.vim
-    fi
-
-    # overwrite symlinks
-    for file in $FILES_TO_SYMLINK; do
-        if [[ $(readlink -f $HOME/$(basename $file)) == $file ]]
-        then
-            echo -e "\t[!] '$HOME/$(basename $file)' already properly linked. Skipping..."
-        else
-            echo -e "\t[+] '$HOME/$(basename $file)' -> '$file'"
-            ln -s -f $file $HOME/$(basename $file)
-        fi  
-    done
-else
-    echo "[-] Skipping..."
-fi
 
 #### edit Vim
 echo -e "\n========================= vim config ========================="
@@ -211,3 +162,60 @@ then
 else
     echo "[-] Skipping..."
 fi
+
+
+#### Gnome Customizations for Desktop
+# echo -e "\n==================== Gnome Customizations ===================="
+# prompt "install Gnome Customizations"
+# 
+# if [[ $prompt_result -eq 1 ]]
+# then
+#     install "gnome-tweaks"
+#     install "papirus-icon-theme"
+#     install "grub-customizer"
+#     install "flameshot"
+#     install "gnome-shell-extensions"
+#     $SCRIPT_DIR/gogh/themes/vs-code-dark-plus.sh 
+# 
+#     # Check if repo already hasn't added
+#     if [[ $(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep papirus/papirus &> /dev/null) -eq 1 ]]
+#     then
+#         echo "[*] Adding repo 'ppa:papirus/papirus'"
+#         sudo add-apt-repository ppa:papirus/papirus
+#     fi
+# 
+#     if [[ -d "/usr/share/themes/Orchis" ]] # lazy check
+#     then 
+#         echo "[-] 'Orchis-theme' is already installed! Skipping..."
+#     else
+#         echo "[*] Installing 'Orchis-theme'"
+#         sudo $SCRIPT_DIR/Orchis-theme/install.sh -d /usr/share/themes --tweaks compact &> /dev/null
+#     fi
+# 
+#     DEST_DIR="/usr/share/icons/"
+#     if [[ -d $DEST_DIR/Vimix-cursors ]] # lazy check
+#     then
+#         echo "[-] 'Vimix-cursors' is already installed! Skipping..."
+#     else
+#         echo "[*] Installing 'Vimix-cursors'"
+#         sudo cp -r $SCRIPT_DIR/Vimix-cursors/dist $DEST_DIR/Vimix-cursors
+#         sudo cp -r $SCRIPT_DIR/Vimix-cursors/dist-white $DEST_DIR/Vimix-white-cursors
+#     fi
+# 
+#     if [[ -d "/usr/share/grub/themes/tela" ]] # lazy check
+#     then
+#         echo "[-] 'grub2-themes' is already installed! Skipping..."
+#     else
+#         echo "[*] Installing 'grub2-themes'"
+#         sudo $SCRIPT_DIR/grub2-themes/install.sh -s 1080p -t tela &> /dev/null
+#     fi
+#     
+#  
+#     # TODO add support for:
+#         # https://extensions.gnome.org/extension/1503/tray-icons/
+#         # https://extensions.gnome.org/extension/921/multi-monitors-add-on/
+#         # https://extensions.gnome.org/extension/307/dash-to-dock/
+#         # https://extensions.gnome.org/extension/906/sound-output-device-chooser/
+# else
+#     echo "[-] Skipping..."
+# fi
